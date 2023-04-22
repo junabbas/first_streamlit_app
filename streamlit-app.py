2826fc576e6616643305d4757ae7cd939197b9f6
junabbas <71361560+junabbas@users.noreply.github.com>
2023-04-22 06:53:15 -0400

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -1,6 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
 streamlit.header('Breakfast menu')
 streamlit.text ('Omega 3 & Blueberry Oatmeal')
 Streamlit.text('Kale, Spinach & Rocket Smoothie')
